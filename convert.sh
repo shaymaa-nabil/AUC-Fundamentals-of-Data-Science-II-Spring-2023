176aff27a97ff64648163b316f75d99449c79b6f
Ahmed Moustafa <amoustafa@aucegypt.edu>
2023-02-06 19:35:01 +0200

    Initial commit

--- convert.sh
+++ convert.sh
@@ -1,2 +1,5 @@
 jupyter nbconvert notebooks/*.ipynb --to pdf --output-dir pdfs/
 jupyter nbconvert notebooks/*.ipynb --to slides --output-dir slides/
+
+
+jupyter nbconvert lecture01.ipynb --to slides --stdout  > lecture01.html
