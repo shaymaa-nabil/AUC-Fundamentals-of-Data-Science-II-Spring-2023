c25517a0e276a8e9316b30d4e991e88788539373
Ahmed Moustafa <amoustafa@aucegypt.edu>
2023-02-06 18:30:12 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AUC Data Science II Spring2023
+
+- Lecture 00 - Course Introduction
+- Lecture 01 - Introduction to Python [Notebook](lecture01.ipynb) | [Slides]() | [PDF](pdfs/lecture01.pdf)
+
--- convert.sh
+++ convert.sh
@@ -1,4 +1,2 @@
-# jupyter nbconvert notebooks/*.ipynb --to pdf --output-dir pdfs/
-
+jupyter nbconvert notebooks/*.ipynb --to pdf --output-dir pdfs/
 jupyter nbconvert notebooks/*.ipynb --to slides --output-dir slides/
-
